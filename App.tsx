e255fe92209ad52d1b74fc6f68cadaae731767e4
Obinna Okoro <obinna.okoroeugene@gmail.com>
2021-06-27 00:20:11 +0100

    majore code refactor

--- App.tsx
+++ App.tsx
@@ -9,15 +9,11 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import AppNavigators from './src/navigation/navigation';
-import * as eva from '@eva-design/eva';
-import { EvaIconsPack } from '@ui-kitten/eva-icons';
-import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
 import { Provider } from 'react-redux';
 import store from './src/store/createStore';
 import { navigationRef } from './src/navigation/NavigationService';
 import Config from 'react-native-config';
 import StorybookUI from './storybook'
-import { NativeBaseProvider } from 'native-base';
 
 function App() {
   return (
