c4b9e9e50c53d7e0f7bd7b83dd7801303367e88e
Obinna Okoro <obinna.okoroeugene@gmail.com>
2021-06-26 21:25:28 +0100

    remove ui kit

--- App.tsx
+++ App.tsx
@@ -6,31 +6,29 @@
  * @flow strict-local
  */
 
- import React from 'react';
- import { NavigationContainer } from '@react-navigation/native';
- import AppNavigators from './src/navigation/navigation';
- import * as eva from '@eva-design/eva';
- import { EvaIconsPack } from '@ui-kitten/eva-icons';
- import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
- import { Provider } from 'react-redux';
- import store from './src/store/createStore';
- import { navigationRef } from './src/navigation/NavigationService';
- import Config from 'react-native-config';
- import StorybookUI from './storybook'
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import AppNavigators from './src/navigation/navigation';
+import * as eva from '@eva-design/eva';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
+import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
+import { Provider } from 'react-redux';
+import store from './src/store/createStore';
+import { navigationRef } from './src/navigation/NavigationService';
+import Config from 'react-native-config';
+import StorybookUI from './storybook'
+import { NativeBaseProvider } from 'native-base';
 
- function App() {
-   return (
-     <>
-       <IconRegistry icons={EvaIconsPack} />
-       <Provider store={store}>
-         <NavigationContainer ref={navigationRef}>
-           <ApplicationProvider {...eva} theme={eva.light}>
-             <AppNavigators />
-           </ApplicationProvider>
-         </NavigationContainer>
-       </Provider>
-     </>
-   );
- }
- 
- export default Config.LOAD_STORYBOOK === 'true' ? StorybookUI : App;
+function App() {
+  return (
+    <>
+      <Provider store={store}>
+        <NavigationContainer ref={navigationRef}>
+          <AppNavigators />
+        </NavigationContainer>
+      </Provider>
+    </>
+  );
+}
+
+export default Config.LOAD_STORYBOOK === 'true' ? StorybookUI : App;
