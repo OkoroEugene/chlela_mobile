1fa87f630b4b68925f2745b7664f2ccd4fe777a2
Obinna Okoro <obinna.okoroeugene@gmail.com>
2021-06-28 12:26:13 +0100

    minor fix

--- src/components/Input/InputStyle.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
-
-export default StyleSheet.create({
-    input: {
-        fontFamily: "DMSans-Regular",
-        marginVertical: 2,
-    },
-})
